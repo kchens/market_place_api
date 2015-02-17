f9b02e388c93915b09a661596a47aaf84ed62766
kchens <kkch3n@gmail.com>
2015-02-17 15:34:47 -0800

    Adds session controller create action

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::SessionsController < ApplicationController
+
+  def create
+    user_password = params[:session][:password]
+    user_email = params[:session][:email]
+    user = user_email.present? && User.find_by(email: user_email)
+
+    if user.valid_password? user_password
+      # store: false ensures that server needs a token for every client request
+      sign_in user, store: false
+      user.generate_authentication_token!
+      user.save
+      render json: user, status: 200, location: [:api, user]
+    else
+      render json: { errors: "Invalid email or password" }, status: 422
+    end
+  end
+end
