c4c53a11f07b007bed9cb8047600c3a0ded03b3f
kchens <kkch3n@gmail.com>
2015-02-17 16:49:19 -0800

    Adds authorization for the users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -60,8 +60,10 @@ describe Api::V1::UsersController do
     context "when is successfully updated" do
       before(:each) do
         @user = FactoryGirl.create :user
+        api_authorization_header(@user.auth_token)
         patch :update, { id: @user.id,
                          user: { email: "newmail@example.com"} }
+
       end
 
       it "renders the json representation for the updated user" do
@@ -75,6 +77,7 @@ describe Api::V1::UsersController do
     context "when is not created" do
       before(:each) do
         @user = FactoryGirl.create :user
+        api_authorization_header(@user.auth_token)
         patch :update, { id: @user.id,
                          user: { email: "bademail.com" } }
       end
@@ -84,7 +87,7 @@ describe Api::V1::UsersController do
         expect(user_response).to have_key(:errors)
       end
 
-      it "renders the json errors on whye the user could not be created" do
+      it "renders the json errors on why the user could not be created" do
         user_response = json_response
         expect(user_response[:errors][:email]).to include "is invalid"
       end
@@ -96,6 +99,7 @@ describe Api::V1::UsersController do
   describe "DELETE #destroy" do
     before(:each) do
       @user = FactoryGirl.create :user
+      api_authorization_header( @user.auth_token )
       delete :destroy, { id: @user.id }
     end
 
