b16dd3da599bd9b1e6f86be88d2d5d87ce028c07
kchens <kkch3n@gmail.com>
2015-02-20 00:45:39 -0800

    Adds validator for order with not enough products on stock

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,15 +7,13 @@ class Order < ActiveRecord::Base
 
   validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :user_id, presence: true
+  validates_with EnoughProductsValidator
 
   def set_total!
     self.total = products.map(&:price).sum
   end
 
   def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
-    p "*" * 70
-    p product_ids_and_quantities
-    p "*" * 70
     product_ids_and_quantities.each do |product_id_and_quantity|
       id, quantity = product_id_and_quantity # [1,5]
 
